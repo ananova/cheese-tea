4519d7ad1fd84810e3abc196a6783db7bc66cbb6
Ана Ђорђевић <me@ana.dj>
2019-01-25 14:45:32 +1000

    Standard fixes.

--- App.js
+++ App.js
@@ -1,57 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import {
   Button,
   CameraRoll,
   Image,
   ImagePickerIOS,
   StyleSheet,
-  Text,
   View
-} from 'react-native';
+} from 'react-native'
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   state = { photos: [], image: null }
-  
+
   _handleButtonPress = () => {
     CameraRoll.getPhotos({
       first: 20,
-      assetType: 'Photos',
+      assetType: 'Photos'
     })
-    .then(r => {
-      this.setState({ photos: r.edges });
-    })
-    .catch((err) => {
-        //Error Loading Images
-    });
+      .then(r => {
+        this.setState({ photos: r.edges })
+      })
+      .catch((err) => {
+        console.error(err)
+      })
   };
 
   pickImage = () => {
     // openSelectDialog(config, successCallback, errorCallback);
     ImagePickerIOS.openSelectDialog(
       {},
-      (imageUri) => { this.setState({ image: imageUri }); },
-      (error)    => { console.error(error); }
-    );
+      (imageUri) => { this.setState({ image: imageUri }) },
+      (error) => { console.error(error) }
+    )
   };
 
-  render() {
+  render () {
     return (
       <View style={styles.container}>
-        { this.state.image ? <Image style={{height: 100, width: 100}} source={{uri: this.state.image}} /> : null }
+        { this.state.image ? <Image style={{ height: 100, width: 100 }} source={{ uri: this.state.image }} /> : null }
         {/* { this.state.image ? <Text>{this.state.image}</Text> : null } */}
-        <Button title="Load Images with ImagePickerIOS" onPress={this.pickImage} />
-        <Button title="Load Images with CameraRoll" onPress={this._handleButtonPress} />
+        <Button title='Load Images with ImagePickerIOS' onPress={this.pickImage} />
+        <Button title='Load Images with CameraRoll' onPress={this._handleButtonPress} />
       </View>
-    );
+    )
   }
 }
 
@@ -60,16 +50,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
