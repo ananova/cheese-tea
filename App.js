db158cc1b97e024434e4e81a34a002ae4a6d9636
Ана Ђорђевић <me@ana.dj>
2019-01-25 12:43:18 +1000

    Add buttons to try to choose images!

--- App.js
+++ App.js
@@ -7,21 +7,43 @@
  */
 
 import React, {Component} from 'react';
-import {CameraRoll, Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {
+  Button,
+  CameraRoll,
+  ImagePickerIOS,
+  StyleSheet,
+  View
+} from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = { photos: [] }
+  
+  _handleButtonPress = () => {
+    CameraRoll.getPhotos({
+      first: 20,
+      assetType: 'Photos',
+    })
+    .then(r => {
+      this.setState({ photos: r.edges });
+    })
+    .catch((err) => {
+        //Error Loading Images
+    });
+  };
+
+  pickImage = () => {
+    // openSelectDialog(config, successCallback, errorCallback);
+    ImagePickerIOS.openSelectDialog({}, imageUri => {
+      this.setState({ image: imageUri });
+    }, error => console.error(error));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title="Load Images with ImagePickerIOS" onPress={this.pickImage} />
+        <Button title="Load Images with CameraRoll" onPress={this.pickImage} />
       </View>
     );
   }
